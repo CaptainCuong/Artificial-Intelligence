34c0c192bb13e34d34ef4248ffdfa729e8a67d2b
CaptainCuong <captaincuong1692001@gmail.com>
2022-03-16 02:02:14 +0700

    update car

---
+++ Project/Car Module/test.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+# vid = cv.VideoCapture(0)
+# print(dir(vid))
+# # print(frame.read())
+# for i in range(100):
+# 	ret, frame = vid.read()
+# 	cv.imshow('sdf', frame)
+# 	print(cv.waitKey(1))
+# cv.inRange()
+
+print(help(cv.createTrackbar))
+print(help(cv.getTrackbarPos))
